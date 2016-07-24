b310338ccb4d2f4357a338ef9bc272fa2e047f60
anurooppv <anurooppv@smu.edu.sg>
2016-07-24 19:05:19 +0800

    add initial wifip2p connector

---
+++ ecology/src/main/java/sg/edu/smu/ecology/Wifip2pConnector.java
@@ -0,0 +1,37 @@
+package sg.edu.smu.ecology;
+
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Created by anurooppv on 22/7/2016.
+ */
+public class Wifip2pConnector implements Connector {
+
+
+    @Override
+    public void sendMessage(List<Object> message) {
+        
+    }
+
+    @Override
+    public void addReceiver(Receiver receiver) {
+
+    }
+
+    @Override
+    public void connect(Context activity) {
+
+    }
+
+    @Override
+    public void disconnect() {
+
+    }
+
+    @Override
+    public boolean isConnected() {
+        return false;
+    }
+}
