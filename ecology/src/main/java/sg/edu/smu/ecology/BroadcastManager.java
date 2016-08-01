6fcabb8fa836dc8c8e6cd85236549dfa9bb40768
anurooppv <anurooppv@smu.edu.sg>
2016-08-01 11:59:03 +0800

    update status of wifip2p device connection

--- ecology/src/main/java/sg/edu/smu/ecology/BroadcastManager.java
+++ ecology/src/main/java/sg/edu/smu/ecology/BroadcastManager.java
@@ -21,6 +21,7 @@ public class BroadcastManager extends BroadcastReceiver {
     private WifiP2pManager.Channel channel;
     private Wifip2pConnector wifip2pConnector;
     private Activity activity;
+    private static DeviceStatusListener DeviceStatusListener;
 
     public BroadcastManager(WifiP2pManager manager, WifiP2pManager.Channel channel,
                             Wifip2pConnector wifip2pConnector, Activity activity) {
@@ -31,13 +32,24 @@ public class BroadcastManager extends BroadcastReceiver {
         this.activity = activity;
     }
 
+    public BroadcastManager(WifiP2pManager manager, WifiP2pManager.Channel channel,
+                            Wifip2pConnector wifip2pConnector, Activity activity, DeviceStatusListener DeviceStatusListener) {
+        super();
+        this.manager = manager;
+        this.channel = channel;
+        this.wifip2pConnector = wifip2pConnector;
+        this.activity = activity;
+        BroadcastManager.DeviceStatusListener = DeviceStatusListener;
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        Log.d(TAG, action);
+        Log.d(TAG, "Action "+ action);
 
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
             int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            Log.i(TAG, "state "+state);
             if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
                 Log.d(TAG, "Wifi status is enabled");
             } else {
@@ -49,12 +61,14 @@ public class BroadcastManager extends BroadcastReceiver {
                 Log.i(TAG, "requestpeers");
                 manager.requestPeers(channel, (WifiP2pManager.PeerListListener) activity.getFragmentManager().
                         findFragmentByTag("peerList"));
+                DeviceStatusListener.handleDeviceStatusChange(WifiP2pDevice.INVITED);
             }
         }
         else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
             if (manager != null) {
                 NetworkInfo networkInfo = (NetworkInfo) intent
                         .getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+                Log.i(TAG, "networkinfo "+networkInfo);
                 if (networkInfo.isConnected()) {
                     Log.d(TAG, "Connected to peer network.");
                     manager.requestConnectionInfo(channel, wifip2pConnector);
@@ -66,7 +80,15 @@ public class BroadcastManager extends BroadcastReceiver {
             WifiP2pDevice device = (WifiP2pDevice) intent
                     .getParcelableExtra(WifiP2pManager.EXTRA_WIFI_P2P_DEVICE);
             Log.d(TAG, "Device status -" + device.status);
+            // This is called when device is available(Not connected) and connected
+            DeviceStatusListener.handleDeviceStatusChange(device.status);
+        }
+        else if(WifiP2pManager.EXTRA_WIFI_P2P_DEVICE.equals(action)){
+            Log.i(TAG, "EXTRA_WIFI_P2P_DEVICE ");
         }
     }
 
+    public interface DeviceStatusListener {
+        void handleDeviceStatusChange(int deviceStatus);
+    }
 }
