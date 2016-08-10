acc32f6ff1834cce98d951777319c93b5d309b7d
anurooppv <anurooppv@smu.edu.sg>
2016-08-10 21:29:08 +0800

    add "ecology:connected" test case in EcologyConnectionTest

--- ecology/src/test/java/sg/edu/smu/ecology/EcologyConnectionTest.java
+++ ecology/src/test/java/sg/edu/smu/ecology/EcologyConnectionTest.java
@@ -1,11 +1,11 @@
 package sg.edu.smu.ecology;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.powermock.api.mockito.PowerMockito;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -20,9 +20,7 @@ public class EcologyConnectionTest {
     @Mock
     private MsgApiConnector msgApiConnector;
     @Mock
-    private Ecology ecology;
-    @Mock
-    private Ecology.RoomFactory roomFactory;
+    private Connector.Receiver receiver;
 
     private EcologyConnection ecologyConnection;
 
@@ -32,8 +30,15 @@ public class EcologyConnectionTest {
         ecologyConnection = new EcologyConnection();
     }
 
+    @After
+    public void tearDown() throws Exception {
+        ecologyConnection = null;
+    }
+
+    // To test "ecology:connected" message sending
     @Test
     public void testEcologyConnected() throws Exception {
+        // Add a core connector
         ecologyConnection.addCoreConnector(wifip2pConnector);
         // To capture the argument in the setReceiver method
         ArgumentCaptor<Connector.Receiver> receiverCaptor1 = ArgumentCaptor.forClass(Connector.Receiver.class);
@@ -42,6 +47,7 @@ public class EcologyConnectionTest {
         Connector.Receiver receiver1;
         receiver1 = receiverCaptor1.getValue();
 
+        // Add a dependent connector
         ecologyConnection.addDependentConnector(msgApiConnector);
         // To capture the argument in the setReceiver method
         ArgumentCaptor<Connector.Receiver> receiverCaptor2 = ArgumentCaptor.forClass(Connector.Receiver.class);
@@ -50,19 +56,14 @@ public class EcologyConnectionTest {
         Connector.Receiver receiver2;
         receiver2 = receiverCaptor2.getValue();
 
-        PowerMockito.whenNew(Ecology.class).withArguments(roomFactory, ecologyConnection).thenReturn(ecology);
-        ecology = new Ecology(roomFactory, ecologyConnection);
+        ecologyConnection.setReceiver(receiver);
 
-        // To capture the argument in the setReceiver method
-        ArgumentCaptor<Connector.Receiver> ecologyreceiverCaptor = ArgumentCaptor.forClass(Connector.Receiver.class);
-        verify(ecologyConnection).setReceiver(ecologyreceiverCaptor.capture());
-        // Create a local mock receiver
-        Connector.Receiver ecologyReceiver;
-        ecologyReceiver = ecologyreceiverCaptor.getValue();
-
+        // Connector 1 gets connected
         receiver1.onConnectorConnected();
+        // Connector 2 gets connected
         receiver2.onConnectorConnected();
 
-        verify(ecologyReceiver, times(2)).onConnectorConnected();
+        // Ecology must be connected only once even if there are multiple connectors
+        verify(receiver, times(1)).onConnectorConnected();
     }
 }
