22d35fef2ca098f28768a8967ac4f6092e7a3326
anurooppv <anurooppv@smu.edu.sg>
2016-08-10 21:45:49 +0800

    remove unnecessary test case

--- ecology/src/test/java/sg/edu/smu/ecology/EcologyTest.java
+++ ecology/src/test/java/sg/edu/smu/ecology/EcologyTest.java
@@ -226,35 +226,4 @@ public class EcologyTest {
         verify(room1, times(1)).onEcologyConnected();
 
     }
-
-    @Test
-    public void testEcologyConnected() throws Exception {
-        ecologyConnection.addCoreConnector(wifip2pConnector);
-        // To capture the argument in the setReceiver method
-        ArgumentCaptor<Connector.Receiver> receiverCaptor1 = ArgumentCaptor.forClass(Connector.Receiver.class);
-        verify(wifip2pConnector).setReceiver(receiverCaptor1.capture());
-        // Create a local mock receiver
-        Connector.Receiver receiver1;
-        receiver1 = receiverCaptor1.getValue();
-
-        ecologyConnection.addDependentConnector(msgApiConnector);
-        // To capture the argument in the setReceiver method
-        ArgumentCaptor<Connector.Receiver> receiverCaptor2 = ArgumentCaptor.forClass(Connector.Receiver.class);
-        verify(msgApiConnector).setReceiver(receiverCaptor2.capture());
-        // Create a local mock receiver
-        Connector.Receiver receiver2;
-        receiver2 = receiverCaptor2.getValue();
-
-        // To capture the argument in the setReceiver method
-        ArgumentCaptor<Connector.Receiver> ecologyreceiverCaptor = ArgumentCaptor.forClass(Connector.Receiver.class);
-        verify(ecologyConnection).setReceiver(ecologyreceiverCaptor.capture());
-        // Create a local mock receiver
-        Connector.Receiver ecologyReceiver;
-        ecologyReceiver = ecologyreceiverCaptor.getValue();
-
-        receiver1.onConnectorConnected();
-        receiver2.onConnectorConnected();
-
-        verify(ecologyReceiver, times(2)).onConnectorConnected();
-    }
 }
