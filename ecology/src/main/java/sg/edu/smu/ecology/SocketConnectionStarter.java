0e9402d407618b47a994a66f96f0122fcf2c4a4b
anurooppv <anurooppv@smu.edu.sg>
2016-08-14 23:09:29 +0800

    first draft

--- ecology/src/main/java/sg/edu/smu/ecology/SocketConnectionStarter.java
+++ ecology/src/main/java/sg/edu/smu/ecology/SocketConnectionStarter.java
@@ -4,6 +4,7 @@ import android.os.Handler;
 import android.util.Log;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -17,6 +18,7 @@ public class SocketConnectionStarter extends Thread {
     private Handler handler;
     private InetAddress address;
     private SocketReadWriter socketCreator;
+    private boolean connectedToServer = false;
 
     public SocketConnectionStarter(Handler handler, InetAddress groupOwnerAddress) {
         this.handler = handler;
@@ -29,19 +31,35 @@ public class SocketConnectionStarter extends Thread {
         try {
             socket.setReuseAddress(true);
             socket.bind(null);
-            Log.d(TAG, "connection attempt");
-            socket.connect(new InetSocketAddress(address.getHostAddress(), Settings.SERVER_PORT),
-                    Settings.TIME_OUT);
-            socketCreator = new SocketReadWriter(socket, handler);
-            new Thread(socketCreator).start();
+            while(!connectedToServer) {
+                try {
+                    Log.d(TAG, "connection attempt");
+                    socket.connect(new InetSocketAddress(address.getHostAddress(), Settings.SERVER_PORT),
+                            Settings.TIME_OUT);
+                    socketCreator = new SocketReadWriter(socket, handler);
+                    new Thread(socketCreator).start();
+                    Log.i(TAG, "socketCreator " + socketCreator);
+                    connectedToServer = true;
+                } catch (ConnectException e) {
+                    Log.i(TAG,"Error while connecting. " + e.getMessage());
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                } catch (Exception e){
+                    //Log.i(TAG,"Exception "+e.getMessage());
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
             try {
+                Log.i(TAG, "Socket close");
                 socket.close();
-            } catch (IOException e1) {
-                e1.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            return;
         }
     }
 }
