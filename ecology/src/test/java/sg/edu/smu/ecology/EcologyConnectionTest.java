758cc69ee96379cfb20c869c1afc60222d81510a
anurooppv <anurooppv@smu.edu.sg>
2016-08-15 09:31:22 +0800

    update EcologyConnectionTest - add testIsConnected

--- ecology/src/test/java/sg/edu/smu/ecology/EcologyConnectionTest.java
+++ ecology/src/test/java/sg/edu/smu/ecology/EcologyConnectionTest.java
@@ -1,19 +1,28 @@
 package sg.edu.smu.ecology;
 
+import android.provider.Settings;
+import android.util.Log;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 /**
  * Created by anurooppv on 21/7/2016.
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( {Settings.Secure.class, Log.class} )
 public class EcologyConnectionTest {
 
     @Mock
@@ -29,6 +38,10 @@ public class EcologyConnectionTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         ecologyConnection = new EcologyConnection();
+
+        // Prepare the mock for static classes
+        PowerMockito.mockStatic(Settings.Secure.class);
+        PowerMockito.mockStatic(Log.class);
     }
 
     @After
@@ -72,4 +85,45 @@ public class EcologyConnectionTest {
         // Ecology must be connected only once even if there are multiple connectors
         verify(receiver, times(1)).onConnectorConnected();
     }
+
+    // To test isConnected method
+    @Test
+    public void testIsConnected(){
+        // Add a core connector
+        ecologyConnection.addCoreConnector(wifip2pConnector);
+        // To capture the argument in the setReceiver method
+        ArgumentCaptor<Connector.Receiver> receiverCaptor1 = ArgumentCaptor.forClass(Connector.Receiver.class);
+        verify(wifip2pConnector).setReceiver(receiverCaptor1.capture());
+        // Create a local mock receiver
+        Connector.Receiver receiver1;
+        receiver1 = receiverCaptor1.getValue();
+
+        // Add a dependent connector
+        ecologyConnection.addDependentConnector(msgApiConnector);
+        // To capture the argument in the setReceiver method
+        ArgumentCaptor<Connector.Receiver> receiverCaptor2 = ArgumentCaptor.forClass(Connector.Receiver.class);
+        verify(msgApiConnector).setReceiver(receiverCaptor2.capture());
+        // Create a local mock receiver
+        Connector.Receiver receiver2;
+        receiver2 = receiverCaptor2.getValue();
+
+        ecologyConnection.setReceiver(receiver);
+
+        // Mock isConnected method for wifip2pConnector
+        PowerMockito.when(wifip2pConnector.isConnected()).thenReturn(true);
+        // Connector 1 gets connected
+        receiver1.onConnectorConnected();
+
+        // To check that when only one connector among two is connected, isConnected returns false
+        assertEquals(false, ecologyConnection.isConnected());
+
+        // Mock isConnected method for msgApiConnector
+        PowerMockito.when(msgApiConnector.isConnected()).thenReturn(true);
+        // Connector 2 gets connected
+        receiver2.onConnectorConnected();
+
+        // To check that when all the connectors are connected, isConnected returns true
+        assertEquals(true, ecologyConnection.isConnected());
+    }
+
 }
