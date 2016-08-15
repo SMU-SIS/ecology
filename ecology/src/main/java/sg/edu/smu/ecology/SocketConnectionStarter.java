cb92cf6bf8ddbeb87ca082f9bb99dac6ab1466ec
anurooppv <anurooppv@smu.edu.sg>
2016-08-15 12:18:16 +0800

    implement number of re-connections

--- ecology/src/main/java/sg/edu/smu/ecology/SocketConnectionStarter.java
+++ ecology/src/main/java/sg/edu/smu/ecology/SocketConnectionStarter.java
@@ -3,6 +3,7 @@ package sg.edu.smu.ecology;
 import android.os.Handler;
 import android.util.Log;
 
+import java.io.IOException;
 import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -17,6 +18,7 @@ public class SocketConnectionStarter extends Thread {
     private Handler handler;
     private InetAddress address;
     private SocketReadWriter socketCreator;
+    private Socket socket = null;
 
     public SocketConnectionStarter(Handler handler, InetAddress groupOwnerAddress) {
         this.handler = handler;
@@ -27,11 +29,13 @@ public class SocketConnectionStarter extends Thread {
     public void run() {
         // To record the status of the connection
         boolean connectedToServer = false;
+        int maxNumberOfRconnections = 20;
+        int currentNumberOfReconnections = 0;
 
         // Try connecting till the connection is setup
         while (!connectedToServer) {
             try {
-                Socket socket = new Socket();
+                socket = new Socket();
                 socket.setReuseAddress(true);
                 socket.bind(null);
 
@@ -46,11 +50,30 @@ public class SocketConnectionStarter extends Thread {
                 connectedToServer = true;
             } catch (ConnectException e) {
                 Log.i(TAG, "Error while connecting. " + e.getMessage());
+                currentNumberOfReconnections++;
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e1) {
                     e1.printStackTrace();
                 }
+
+                // Stop reconnecting when the number of attempts reaches max number of reconnections
+                if(currentNumberOfReconnections == maxNumberOfRconnections){
+                    currentNumberOfReconnections = 0;
+                    Log.i(TAG, "Reconnections stopped");
+                    connectedToServer = true;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                try {
+                    if (socket != null && !socket.isClosed()) {
+                        Log.i(TAG, "Socket close ");
+                        socket.close();
+                    }
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                return;
             } catch (Exception e) {
                 e.printStackTrace();
                 return;
