bd23ba252db6260bf70f8f041ac21b56aa874573
Quentin Roy <quentin@quentinroy.fr>
2016-10-07 20:21:47 +0800

    Cosmetics

--- ecology/src/test/java/sg/edu/smu/ecology/EventBroadcasterTest.java
+++ ecology/src/test/java/sg/edu/smu/ecology/EventBroadcasterTest.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Vector;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.never;
