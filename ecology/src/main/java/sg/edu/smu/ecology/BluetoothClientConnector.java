42df0e327b2f7978aaf578b77ceadb2c685775dc
anurooppv <anurooppv@smu.edu.sg>
2016-10-25 14:57:57 +0800

    add BluetoothClientConnector class

---
+++ ecology/src/main/java/sg/edu/smu/ecology/BluetoothClientConnector.java
@@ -0,0 +1,34 @@
+package sg.edu.smu.ecology;
+
+/**
+ * Created by anurooppv on 25/10/2016.
+ */
+
+public class BluetoothClientConnector extends BluetoothConnector implements
+        BluetoothConnector.ServerDisconnection{
+    private static final String TAG = BluetoothClientConnector.class.getSimpleName();
+
+    private BluetoothClientConnectThread bluetoothClientConnectThread;
+
+    @Override
+    public void setupBluetoothConnection() {
+        bluetoothClientConnectThread = new BluetoothClientConnectThread(getBluetoothAdapter(),
+                getPairedDevicesList().get(0), getUuidsRequiredList(), getHandler());
+        bluetoothClientConnectThread.start();
+        setServerDisconnection(this);
+    }
+
+    @Override
+    public void disconnect() {
+        super.disconnect();
+
+        if (bluetoothClientConnectThread != null && !bluetoothClientConnectThread.isInterrupted()) {
+            bluetoothClientConnectThread.interrupt();
+        }
+    }
+
+    @Override
+    public void resetClientConnectThread() {
+        bluetoothClientConnectThread.setConnectedToServer(false);
+    }
+}
