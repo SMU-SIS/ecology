f00ce6c32f0b2e8938d744d2433644d941bd63c5
anurooppv <anurooppv@smu.edu.sg>
2016-12-20 11:45:56 +0800

    update sync data change listener interface

--- ecology/src/main/java/sg/edu/smu/ecology/DataSync.java
+++ ecology/src/main/java/sg/edu/smu/ecology/DataSync.java
@@ -15,37 +15,32 @@ import java.util.Map;
  */
 public class DataSync {
     private static final String TAG = DataSync.class.getSimpleName();
-
+    // Notified when the data has changed.
+    private final SyncDataChangeListener dataChangeListener;
     /**
      * To store the sync data as a key value pair
      */
     private Map<Object, Object> dataSyncValues = new HashMap<>();
+    // Notify when a message needs to be forwarded to the corresponding DataSyncs in the other
+    // devices of the ecology.
     private Connector connector;
 
-    private SyncDataChangeListener syncDataChangeListener = new SyncDataChangeListener() {
-        @Override
-        public void onDataUpdate(List<Object> message) {
-            dataSyncValues.put(message.get(0), message.get(1));
-        }
-    };
-
-    DataSync(Connector connector) {
+    DataSync(Connector connector, SyncDataChangeListener dataChangeListener) {
         this.connector = connector;
-    }
-
-    SyncDataChangeListener getSyncDataChangeListener() {
-        return syncDataChangeListener;
+        this.dataChangeListener = dataChangeListener;
     }
 
     /**
      * Sets the sync data value
      *
      * @param key   the key paired to the sync data
-     * @param value the data
+     * @param value the new data
      */
     public void setData(Object key, Object value) {
+        Object oldValue = dataSyncValues.get(key);
         dataSyncValues.put(key, value);
-        connector.onMessage(new ArrayList<Object>(Arrays.asList(key, value)));
+        connector.onMessage(new ArrayList<>(Arrays.asList(key, value)));
+        dataChangeListener.onDataUpdate(key, value, oldValue);
     }
 
     /**
@@ -58,12 +53,25 @@ public class DataSync {
         return dataSyncValues.get(key);
     }
 
+    /**
+     * Handle message from the corresponding DataSync instances in the other devices of the ecology.
+     *
+     * @param message the content of the message
+     */
+    void onMessage(List<Object> message) {
+        Object key = message.get(0);
+        Object oldValue = dataSyncValues.get(key);
+        Object newValue = message.get(1);
+
+        dataSyncValues.put(key, newValue);
+        dataChangeListener.onDataUpdate(key, newValue, oldValue);
+    }
+
     interface Connector {
         void onMessage(List<Object> message);
     }
 
     interface SyncDataChangeListener {
-        void onDataUpdate(List<Object> message);
+        void onDataUpdate(Object dataId, Object newValue, Object oldValue);
     }
-
 }
